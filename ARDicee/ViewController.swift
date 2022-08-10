a1f2038074b35fe65e9e6cbfd7f50c53e779d485
Barath2803 <1934004aiml@cit.edu.in>
2022-08-10 08:25:16 +0530

    touch detection on plane

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -23,16 +23,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
         
-        // Create a new scene for dice
-//        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
-//
-//        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true){
-//            diceNode.position = SCNVector3(x:0, y:0, z:-0.1)
-//            sceneView.scene.rootNode.addChildNode(diceNode)
-//        }
-//
-//        sceneView.autoenablesDefaultLighting = true
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -52,6 +42,30 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let touchLocation = touch.location(in: sceneView)
+            
+            let result = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+            
+            if let hitResult = result.first {
+                //Create a new scene for dice
+                let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+
+                if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+                    diceNode.position = SCNVector3(
+                        x: hitResult.worldTransform.columns.3.x,
+                        y: hitResult.worldTransform.columns.3.y + diceNode.boundingSphere.radius,
+                        z: hitResult.worldTransform.columns.3.z)
+                    sceneView.scene.rootNode.addChildNode(diceNode)
+               }
+
+               sceneView.autoenablesDefaultLighting = true
+               
+            }
+        }
     }
     
     //Plan detection
